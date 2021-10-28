b957f21d6d0c9a547ba8807973374bdcf2a8ff98
Adam Jagielski <jagielski.adamm@gmail.com>
2021-10-28 09:50:32 +0200

    Clean ups

--- DPLL.c
+++ DPLL.c
@@ -15,7 +15,7 @@ int** copy_cnf(int** cnf);                          // PRODUCES COPY OF CNF FORM
 int** transpose_cnf(int** cnf);                     // TRANSPOSES CLAUSES->VARIABLES CNF FORMULA INTO VARIABLES->CLAUSES T_CNF FORMULA //
 int* find_units(int** cnf);                         // SEARCHES FOR UNIT LITERALS IN CNF FORMULA //
 int* find_pures(int** t_cnf);                       // SEARCHES FOR PURE LITERALS IN T_CNF FORMULA //
-int* concat(int* a, int* b);                        // JOINS TWO ARRAYS, RETURNS OUTCOME REFERENCE, FREES SUBSTRATS
+int* concat(int* a, int* b);                        // JOINS TWO ARRAYS, RETURNS OUTCOME REFERENCE, FREES COMPONENTS //
 int get_sign(int num);                              // RETURNS -1,0 or 1 FOR NEGATIVE, ZERO AND POSITIVE INTEGERS RESPECTIVELY //
 void reduce(int** cnf, int* reducibles);            // REDUCES FORMULAE THROUGHOUT CROSS-OUT RULE BASED ON PASSED ASSUMPTIONS //
 void erase_formula(int** formula, int transposed);  // DEALLOCATES MEMORY UNDER CNF OR T_CNF //
@@ -36,7 +36,6 @@ int** random_cnf(int k, int variables, int clauses);
 
 int START_TIME;
 int HEURISTIC_ID;
-int SMOOTHNESS;
 char* CNF_FILE_NAME;
 
 int main(int argc, char** argv){
